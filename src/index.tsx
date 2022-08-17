900c18542521bb6157d07a50de8e1f4dcaae7b86
Luiz Cláudio <luizcns18@gmail.com>
2022-08-17 06:32:34 -0300

    Async fetch example (selector)

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, {Suspense} from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import Canvas from './Canvas'
 import {ChakraProvider} from '@chakra-ui/react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import {RecoilRoot} from 'recoil'
+import {Async} from './examples/Async'
 
 ReactDOM.render(
     <React.StrictMode>
@@ -12,6 +13,11 @@ ReactDOM.render(
             <ChakraProvider>
                 <Router>
                     <Switch>
+                        <Route path="/examples/async">
+                            {/* <Suspense fallback={<div>Loading...</div>}> */}
+                            <Async />
+                            {/* </Suspense> */}
+                        </Route>
                         <Route>
                             <Canvas />
                         </Route>
